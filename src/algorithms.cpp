3dfd99ab574d9f1a352df99a5c7d413c634fef05
Will Collings <igivuspam@gmail.com>
2018-03-12 13:38:13 +0000

    Pushing Version 0.5.1
    * Add files via upload
    
    * Create settings.txt
    
    * Update README.md
    
    * Add files via upload
    
    * Update makefile
    
    * first push in a while. updates are numerous
    
    * Update README.md
    
    * update. runs on JSON now
    
    * update. runs on JSON now
    
    * the makefile works now
    
    * utilities fixed
    
    * added message logging
    
    * moved to src, only copied here
    
    * moved to src/include
    
    This is an artifact of an old file structure
    
    * moved to src
    
    This is an artifact of an old file structure
    
    * moved to src
    
    This is an artifact of an old file structure
    
    * blank file shouldn't exist

---
+++ src/algorithms.cpp
@@ -0,0 +1,116 @@
+//ALGORITHMS
+//has the algorithms for actually making the passwords
+
+#include "include/site.hpp"
+#include "utilities.cpp"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+using json= nlohmann::json;
+
+//generate a long password: 3 words strung together
+void longpw(site info)
+{
+	srand(time(NULL));
+	ifstream in;
+	in.open(settings.at("words_file").get<std::string>());
+	int i=0;
+	string pw, temp;
+	vector<string> list;
+	while (in)
+	{
+		in >>temp;
+		list.push_back(temp);
+	}
+	if (settings.at("verbose").get<bool>())
+		cout <<"read in " <<i <<" entries\n";
+	in.close();
+	int r=rand()%list.size();
+	for (int x=0; x <3; ++x)
+	{
+		pw+=list[r];
+		if (x <2) pw+="_";
+		r=rand()%list.size();
+	}
+	info.password=pw;
+	cout <<info.website <<": " <<info.uname <<'\n' <<pw <<'\n';
+	logNewEntry(info);
+
+}
+
+//generate a normal password
+void shortpw(site info)
+{
+	time_t t=time(0);
+	struct tm * now = localtime(& t);
+	string pw="";
+		
+	pw+=to_string(convertToNumber(info.website[0]));
+	pw+=to_string(convertToNumber(info.website[1]));
+	pw+=toupper(info.website[2]);
+	if (now->tm_mon+1 < 10)
+	{
+		pw+="0";
+		pw+=to_string(now->tm_mon+1);
+	}
+	else pw+=to_string(now->tm_mon+1);
+	if (now->tm_mday+1 < 10)
+	{
+		pw+="0";
+		pw+=to_string(now->tm_mday+1);	
+	}
+	else pw+=to_string(now->tm_mday+1);
+	try
+	{
+		if (info.website.size() < 3)
+			throw -1;
+		pw+=info.website.substr(info.website.size()-3,3);
+	}
+	catch (int e)
+	{
+		switch (e)
+		{
+			case -1:pw+="com";
+		
+		}
+	}
+	cout <<info.website <<": " <<info.uname <<'\n' <<pw <<'\n';
+	info.password=pw;
+	logNewEntry(info);
+}
+
+//x number of random digits 
+void randpw(site info)
+{
+	//valid between 50 143
+	string pw;
+	srand(time(NULL));
+	int r=rand()%143;
+	int i=0;
+	while (i <info.length)
+	{
+		r=rand()%143;
+		if (r > 50)
+			r-=17;
+		else continue;
+		if (info.illegal.size() > 0)
+		{
+			if (any_of(info.illegal.begin(), info.illegal.end(), [&r] (char illegal) {return static_cast<char>(r)==illegal;}))
+			{
+				cout <<"found " <<static_cast<char>(r) <<endl;
+				continue;
+			}
+		}
+		pw+=static_cast<char>(r);
+		++i;
+		
+	}
+	info.password=pw;
+	cout <<info.website <<": " <<info.uname <<'\n' <<pw <<'\n';
+	logNewEntry(info);
+}
---
+++ src/main.cpp
@@ -0,0 +1,103 @@
+//MAIN
+//has the main function, and that's it
+
+#include <string>
+#include "include/site.hpp"
+#include "algorithms.cpp"
+using namespace std;
+
+enum selector:int{Short=0, Long=1, Rand=2};
+void create_password(site, selector);
+
+int main(int argc, char * argv[])
+{
+	systemLog.open("/var/log/pw-gen.log", std::ofstream::app);
+	logMessages(0);
+	site temp;
+	if (!readIn()) return -1;
+	temp.illegal=settings.at("illegal").get<std::string>();
+	temp.length=settings.at("default_length").get<int>();
+	bool generate=false;
+	selector flag;
+	if (argc < 2)
+	{
+		help();
+		return 0;
+	}
+	for (int i=1; i < argc; ++i)
+	{
+	 	if (argv[i][0]=='-')
+			switch(argv[i][1])
+			{
+				case 'L':
+				{
+					flag=Long;
+					generate=true;
+				}
+					break;
+				case 'S':
+				{
+					flag=Short;
+					generate=true;
+				}
+					break;
+				case 'R':
+				{
+						flag=Rand;
+						generate=true;
+				}
+					break;
+				case 'i':
+				{
+					temp.illegal=argv[++i];
+				}
+					break;
+				case 'n':
+				{
+					//read in notes. will have to be a prompt
+				}
+					break;
+				case 'f':
+				{
+					exists(argv[++i]);
+				}
+					break;
+				case 'u':
+				{
+					temp.uname=argv[++i];
+				}
+					break;
+				case 'l':
+				{
+					temp.length=atoi(argv[++i]);
+				}
+					break;
+				case 'e':
+					temp.email=argv[++i];
+			}
+		else temp.website=argv[i];
+		//cout <<i <<'=' <<argv[i] <<'\n';
+	}
+	//cout <<temp.website <<'\n';
+	if (generate)
+		create_password(temp, flag);
+	write();
+	systemLog.close();
+}
+
+void create_password(site temp, selector flag)
+{
+	if (!exists(temp.website))
+	{
+		logMessages(1);
+		switch (flag)
+		{
+			case Short: shortpw(temp);
+				break;
+			case Long: longpw(temp);
+				break;
+			case Rand: randpw(temp); 
+		}
+	}
+
+}
